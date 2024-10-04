af29f5104a25aaab5958743da328f9d89574e1e2
MLarbo37 <mlarbo37@gmail.com>
2024-10-04 22:28:20 +0000

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+chainWebpack: (config) => {
+  config.module
+    .rule('vue')
+    .use('vue-loader')
+    .tap((options) => {
+      // https://v3.vuejs.org/guide/migration/custom-elements-interop.html#autonomous-custom-elements
+      options.compilerOptions = {
+        isCustomElement: (tag) => tag.startsWith('scale-'),
+      };
+      return options;
+    });
+};
